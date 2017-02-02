ba87722be1d5c1c83c56acde48782eef6ffeeaf8
levisc8 <levisc8@gmail.com>
2017-02-02 14:48:57 +0100

    Musdata documentation

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Musdata
+#'
+#' A dataset containing 400 presence absence records of Mus musculus (a house mouse)
+#'
+#'@format A data frame with 400 rows and 5 columns
+#'  \describe{
+#'    \item{musculus}{Presence/absence records of Mus musculus}
+#'    \item{pollution}{numberic - describes degree of pollution in corresponding grid cell}
+#'    \item{exposure}{numeric - describes degree of exposure for each grid cell}
+#'    \item{x}{integer - x coordinate for each grid cell}
+#'    \item{y}{numeric - y coordinate for each grid cell}
+#'  }
+#'
