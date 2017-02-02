fb6eddb82007a441fced1f6b59520ace67d9a09f
levisc8 <levisc8@gmail.com>
2017-02-02 16:42:14 +0100

    Add hook data set to /data folder
    Update data help file

--- R/data.R
+++ R/data.R
@@ -1,15 +1,29 @@
 #' Mus musculus data set
 #'
-#' A dataset containing 400 presence absence records of Mus musculus (a house mouse)
+#' A data frame containing presence absence records of Mus musculus (a house mouse)
 #'
 #'@format A data frame with 400 rows and 5 columns
 #'  \describe{
-#'    \item{musculus}{Presence/absence records of Mus musculus}
+#'    \item{musculus}{integer - Presence/absence records of Mus musculus}
 #'    \item{pollution}{numberic - describes degree of pollution in corresponding grid cell}
 #'    \item{exposure}{numeric - describes degree of exposure for each grid cell}
-#'    \item{x}{integer - x coordinate for each grid cell}
-#'    \item{y}{numeric - y coordinate for each grid cell}
+#'    \item{x}{integer - x-coordinates for each grid cell}
+#'    \item{y}{numeric - y-coordinates for each grid cell}
 #'  }
 #'
-
 "musdata"
+
+#'hook data set
+#'
+#'A data frame containing actual presence absence data and predicted probability
+#'of occurrence
+#'
+#'@format A data frame with 100 rows and 4 columns
+#'  \describe{
+#'    \item{actuals}{integer - Presence/absence records}
+#'    \item{predictions}{numeric - predicted probabilities of occurence}
+#'    \item{x}{integer - x-coordinates for each grid cell}
+#'    \item{y}{numeric - y-coordinates for each grid cell}
+#'
+#'  }
+"hook"
