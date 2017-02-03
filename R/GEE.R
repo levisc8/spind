446831e47cc98f0c785faf961ac8dba89bd01843
levisc8 <levisc8@gmail.com>
2017-02-03 13:53:42 +0100

    create separate file for wrm.moran

--- R/GEE.R
+++ R/GEE.R
@@ -347,23 +347,6 @@ qic <- function(formula,data,family,mu,var.robust,var.indep.naive){
 
 }
 
-
-wrm.moran <- function(lim1=0,increment=1){
-  ###############################################################################
-  # Description
-  # Auxiliary function for specifying the Moran's I calculation.
-  # Typically only used internally by WRM.
-  # Arguments:
-  # lim1      lower limit for first bin
-  # increment increment (=1 and lim1=0 is conform to correlog{ncf})
-  #
-  # Value:     A list with components named as the arguments.
-  ###############################################################################
-  moran.list <- list(lim1=lim1,increment=increment)
-  return(moran.list)
-}
-
-
 dat.nn <- function(data,coord,n){
   ###############################################################################
   # Description
