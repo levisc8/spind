28f641bbc49114130ee982c2e9415e50d96da1dd
levisc8 <levisc8@gmail.com>
2017-02-20 10:34:55 +0100

    rename function to avoid method conflict

---
+++ R/plot_covar.R
@@ -0,0 +1,84 @@
+#' Plot wavelet variance/covariance
+#'
+#' @description The plot is based on a wavelet variance or covariance analysis
+#' and will work where \code{\link{wavevar}} or \code{\link{wavecovar}} as function
+#' is supported. The scale-dependent results are graphically displayed.
+#'
+#' @param formula   With specified notation according to names in data frame
+#' @param data      Data frame
+#' @param coord     A matrix of 2 columns with
+#'  corresponding x,y-coordinates which have to be integer
+#' @param wavelet  Type of wavelet: \code{haar}, \code{d4}, or \code{la8}
+#' @param wtrafo   Type of wavelet transform: \code{dwt} or \code{modwt}
+#' @param plot      Either \code{var} for wavelet variance analysis
+#'           or \code{covar} for wavelet covariance analysis
+#'
+#' @return    A list containing a vector of results
+
+
+covar.plot<-function(formula,data,coord,wavelet="haar",wtrafo="dwt",
+                     plot="covar"){
+
+  x<-coord[,1]
+  y<-coord[,2]
+  X<-model.matrix(formula,data)
+  namvar<-dimnames(X)[[2]]
+  if(namvar[1]!="(Intercept)") nvar1<-1
+  if(namvar[1]=="(Intercept)") nvar1<-2
+  nvar2<-dim(X)[2]
+  namresp<-as.character(formula[[2]])
+  resp<-model.frame(formula,data)[[1]]
+  wvar0<-wavevar(resp,x,y,wavelet=wavelet,wtrafo=wtrafo)
+  nscale<-length(wvar0)
+
+  if(plot=="var"){
+    # Variance
+    wvar<-matrix(NA,nvar2,nscale)
+    for (kk in nvar1:nvar2){
+      wvar[kk,]<-wavevar(X[,kk],x,y,wavelet=wavelet,wtrafo=wtrafo)
+    }
+    plot(wvar0[1:nscale],type="b",ylim=c(-.1,.9),pch=16,
+         ylab="Variance", xlab="Level",
+         main=paste(paste(wavelet,wtrafo)," - wavelet variance") )
+    for(kk in nvar1:nvar2){
+      points(wvar[kk,1:nscale],pch=kk,type="b")
+    }
+    leg<-c(namvar[nvar1:nvar2],namresp)
+    v<-nvar1:nvar2
+    v<-c(v,16)
+    legend(2.5,0.8,leg,pch=v)
+  }
+
+  if(plot=="covar"){
+    # Covariance
+    wcvar<-matrix(NA,nvar2,nscale)
+    for (kk in nvar1:nvar2){
+      wcvar[kk,]<-wavecovar(resp,X[,kk],x,y,wavelet=wavelet,wtrafo=wtrafo)
+    }
+    plot(wcvar[nvar1,1:nscale],type="b",ylim=c(-.1,.6),pch=nvar1,
+         ylab="Covariance", xlab="Level",
+         main=paste(paste(wavelet,wtrafo)," - wavelet covariance") )
+    for(kk in (nvar1+1):nvar2){
+      points(wcvar[kk,1:nscale],pch=kk,type="b")
+    }
+    leg<-rep(NA,nvar2-nvar1+1)
+    for (kk in nvar1:nvar2){
+      leg[kk]<-paste(namresp,namvar[kk],sep=" --- ")
+    }
+    if(nvar1==2) leg<-leg[-1]
+    v<-nvar1:nvar2
+    legend(2.5,0.5,leg,pch=v)
+  }
+
+  if(plot=="var") {
+    res<-rbind(wvar0,wvar)
+    rownames(res)<-c(namresp,namvar)
+  }
+  if(plot=="covar") {
+    res<-wcvar
+    rownames(res)<-paste(namresp,namvar,sep="-")
+  }
+
+  fit<-list(result=res)
+  fit
+}
