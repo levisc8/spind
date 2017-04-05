0358837d4856406d0608aab956b7950ccc32396b
levisc8 <levisc8@gmail.com>
2017-04-05 10:42:14 +0200

    Add package citation

---
+++ inst/CITATION.R
@@ -0,0 +1,17 @@
+citHeader('To cite spind in publications use:')
+
+citEntry(entry="Article",
+         title = "Spind: a package for computing spatially corrected accuracy measures.",
+         author = personList(as.person("Gudrun Carl"),
+                             as.person("Ingolf Kuehn")),
+         journal = "Ecography",
+         year = "2016",
+         DOI = "10.1111/ecog.02593",
+
+
+         textVersion = paste("Carl G, Kuehn I (2016)",
+                             "Spind: a package for computing spatially corrected accuracy measures.",
+                             "Ecography",
+                             "DOI: 10.1111/ecog.02593")
+
+)
